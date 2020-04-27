f68b6e162f1cecd72ce6278cf33b9b2fb8c577b4
HelloNetWorld <42215383+HelloNetWorld@users.noreply.github.com>
2020-04-27 11:35:25 +0300

    Добавлена иконка приложения

--- Crimson/obj/Debug/Crimson_Content.g.i.cs
+++ Crimson/obj/Debug/Crimson_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/pubgicon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("circle_red_3034.ico")]
 
 
